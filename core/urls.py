e009c383a16291909ec51fef4eb7bca8c1ed3461
Anusha Batra <anushabatra46@gmail.com>
2020-08-26 20:51:18 +0500

    resolve login issue

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,19 @@ from django.contrib import admin
 from core import views
 from allauth.account import views as allauth_views
 from allauth.account.views import LoginView as lv
+from django.conf import settings
+
 
 
 
 urlpatterns = [
     path('log', views.log),
     path('dash', views.dash),
-    path('dashboard', views.dashboard, name="dash_log"),
+    path('dashboard', views.dash, name="dash_log"),
     path("login/", lv.as_view(), name="account_login"),
     path('',allauth_views.LoginView.as_view()),
-    path('logout',allauth_views.LogoutView.as_view()),
-    path('launch', views.launch)
+    
+    path('launch', views.launch),
+    path(r'^logout/$', allauth_views.LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout')
     
 ]
